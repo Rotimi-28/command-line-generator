8045b61ea14c800b57b29688c04a3f4b0a6a77a2
Rotimi.E <matanmearoja@yahoo.com>
2022-05-02 18:11:12 -0700

    Original initial commit

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+//const axios = require("axios");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+//Prompt user for github username
+inquirer.prompt([
+    {
+        name: "username",
+        message: "Enter your GiHub username",
+        default: "Rotimi-28"
+    },
+    {
+        name: "title",
+        message: "Pleasea, enter your project Title",
+        default: "Readme-Generator"
+    },
+    {
+        name: "description",
+        message: "Please, briefly descrioption of your project",
+        default: "An automated template that produces and orgnized and formatted readme.md file"
+    },
+    {
+        name: "installation",
+        message: "what are the steps for the installation?",
+        default: "Run 'npm install' from terminal to add the dependencies.",
+    },
+    {
+        name: "usesage",
+        message: "How should to your project be used?",
+        default: "The CLI application should be used from the terminal, once dependencies are have already been installed. Open therminal and run 'node index.js' then follow the prompts.",
+    },
+    {
+        name: "license",
+        message: "Indicate project licence(s):",
+        default: "None"
+    },
+    {
+        name: "contributors",
+        message: "List of contributor on this project",
+        default: "None"
+    },
+    {
+        name: "tests",
+        message: "Explain how to run testing :",
+        default: "Currently there are no tests available."
+    },
+    {
+        name: "Questions",
+        message: "Remaining questions",
+        default: "None"
+    }
+])
